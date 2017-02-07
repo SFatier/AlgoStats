3491f29fc2538dc6770b9c54222d79409b2bcc96
Segolene Fatier <fatier-segolene@outlook.com>
2017-02-07 10:16:40 +0100

    blem avec trifusion

--- algorithms.js
+++ algorithms.js
@@ -90,36 +90,43 @@ function tri_shell(t){
 
 function tri_fusion (tab){
 	cpt=tab.length;
+	j = 0;
 	if( cpt <= 1 ){
 		return;	
 	} 
     else
     {
-        tab1 = array();
-        tab2 = array();
-        for( i = 0; $i < cpt; i++) {
-            if( $i < cpt / 2 )
-                tab1[] = tableau[i];
+        tab1 = new Array;
+        tab2 = new Array;
+        for( i = 0; i < cpt; i++) {
+            if( i < cpt / 2 )
+            {
+                tab1[j] = tab[i];
+            }
             else
-                tab2[] = tableau[i];
+            {
+            	tab2[j] = tab[i];
+            }
+            j++;
         }
     	  // Appel la fonction tri récursivement
-        triFusion(tab1);
-        triFusion(tab2);
+        tri_fusion(tab1);
+        tri_fusion(tab2);
         // Fusionne les petits tableaux en plus grand
-        fusionner(tab1,tab2,tableau);
+        fusionner(tab1,tab2,tab);
     }
-	result[0]= tableau;
+    result = new Array;
+	result[0]= tab;
 	result[1]= cpt; //nb de cycle 
 	return result; //tab de tab avec nb cycle en result[1]
 }
 
- public function fusionner ( $tab1, $tab2, &$tab )
+function fusionner ( tab1, tab2, $tab )
 {
         i = 0;
         i1 = i2 = 0;
         // Fusionne les petits tableaux dans le plus grand
-        while( i1 < tab1.length ) && i2 < tab2.lenth ) ) {
+        while( i1 < tab1.length && i2 < tab2.lenth ) {
             if( tab1[ i1 ] < tab2[ i2 ] ) // On compare ici
                 tab[ i ] = tab1[ i1++ ];
             else
@@ -134,22 +141,26 @@ function tri_fusion (tab){
         while( i2 < tab2 ) {
             tab[ i ] = tab2[ i2++ ];
             i++;
+        }
 }
 
-function tri_peigne(tableau){
+function tri_peigne(tableau)
+{
 	gap = tableau.length;
 	permutation = true;
 
 	cpt = 0; //declaration nb cycle
 
-	while (permutation || gap>1){
+	while (permutation || gap>1)
+	{
 		permutation = false;
 		gap = gap / 1.3;
-		if (gap<1) gap=1;
-		for (en_cours=0;en_cours<20-gap;en_cours++) {
-			if (tableau[en_cours]>tableau[en_cours+gap]){
+		if (gap<1) { gap=1 ; }
+		for (en_cours=0;en_cours<20-gap;en_cours++) 
+		{
+			if (tableau[en_cours]>tableau[en_cours+gap])
+			{
 				permutation = true;
-				// on echange les deux elements
 				temp = tableau[en_cours];
 				tableau[en_cours] = tableau[en_cours+gap];
 				tableau[en_cours+gap] = temp;
@@ -157,15 +168,13 @@ function tri_peigne(tableau){
 			cpt++; //nb cycle
 		}
 	}
-
-	result = array();
-	result[0]= t;
+	result = new Array;
+	result[0]= tableau;
 	result[1]= cpt; //nb de cycle 
-
-	return result; 	 //tab de tab avec nb cycle en result[1]
+	return result; //tab de tab avec nb cycle en result[1]
 }
 
-function tri_rapide(array){
+/*function tri_rapide(array){
  	if(tableau.length < 2 ) {
         return tableau;
     }
@@ -181,3 +190,4 @@ function tri_rapide(array){
     } 
     return array_merge(static::triRapide(left), array(pivot_key => pivot), static::triRapide(right));
 }
+*/
